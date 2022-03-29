387ed173a22fadf1935787f5163dc558e9027486
David Mata <52453479+davidrobert99@users.noreply.github.com>
2022-03-30 00:15:13 +0100

    Demo Card

--- Demo_React/Routes/src/Components/Login.js
+++ Demo_React/Routes/src/Components/Login.js
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export function Login(props) {
+  const [user, setUser] = useState();
   const navigate = useNavigate();
 
   return (
